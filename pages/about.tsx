205c466ebffbe650f43796c1e2a8fb1bb87c01d1
nworb9 <emma.nworb@gmail.com>
2022-05-07 20:24:20 -0400

    Implemented grid on about and home page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import headshot from '../public/assets/headshot.png'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Image from "next/image";
+import fall from "../public/assets/accord-fall-172.jpg";
+import headshot from "../public/assets/headshot.png";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-      <article className={styles.article} >
-        <div id="para1">
+        <article className={styles.article}>
+          <Image
+            alt="fall shot with dark brown pool house in the foreground, shot from the side"
+            src={fall}
+            width={500}
+            height={500}
+            objectFit="contain"
+          />
           <h2 id="whoWeAre">who we are</h2>
           <p>
             At Bone Hollow Studio we specialize in curated and welcoming homes.
@@ -17,34 +23,39 @@ const Home: NextPage = () => {
             Homes & Renovations, we offer clean and gracious motifs that are
             thoughtfully sited in a quiet, introspective setting.
           </p>
-        </div>
-        <h2 id="owner">owner</h2>
-        <section className="flexRow">
-          <div className="flexRow">
-            <p id="para1Text">
-              Erika grew up in Berlin, Germany and the American Midwest. A graduate
-              of Northwestern University, she has over 15 years of experience in
-              producing commercials and special effects for an illustrious client
-              base, amongst them Nike, Coca-Cola, McDonald's, Chevrolet, and
-              Anheuser Busch. Erika has also worked in development and event
-              planning for non-profits, including Jersey Battered Women's
-              Service.<br />
-              Erika's latest work with the New York-based interior designer, Amanda
-              Rehbein Studio, has segued into the creation of her company, Bone
-              Hollow Studio. With a background in advertising production and
-              styling, Erika combines her passion for creative design with a natural
-              aptitude for project management. She believes one's home should be
-              unique, reflecting a deeply personal connection with the land. Erika's
-              goal is to create spaces that are at once impactful and livable,
-              timeless and comfortable.
-            </p>
-          </div>
-          <div className="flexRow"><Image alt='headshot' src={headshot} /></div>
-        </section>
+        </article>
+
+        <article className={styles.article}>
+          <h2 id="owner">owner</h2>
+          <p id="para1Text">
+            Erika grew up in Berlin, Germany and the American Midwest. A
+            graduate of Northwestern University, she has over 15 years of
+            experience in producing commercials and special effects for an
+            illustrious client base, amongst them Nike, Coca-Cola, McDonald's,
+            Chevrolet, and Anheuser Busch. Erika has also worked in development
+            and event planning for non-profits, including Jersey Battered
+            Women's Service.
+            <br />
+            Erika's latest work with the New York-based interior designer,
+            Amanda Rehbein Studio, has segued into the creation of her company,
+            Bone Hollow Studio. With a background in advertising production and
+            styling, Erika combines her passion for creative design with a
+            natural aptitude for project management. She believes one's home
+            should be unique, reflecting a deeply personal connection with the
+            land. Erika's goal is to create spaces that are at once impactful
+            and livable, timeless and comfortable.
+          </p>
+          <Image
+            alt="headshot"
+            src={headshot}
+            width={500}
+            height={500}
+            objectFit="contain"
+          />
         </article>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,66 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import pigPic from '../public/assets/sample.jpeg'
-import backyard from '../public/assets/accord-fall-172.jpg'
-import accordSring209 from '../public/assets/accord-spring-209.jpg'
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import pigPic from "../public/assets/sample.jpeg";
+import backyard from "../public/assets/accord-fall-172.jpg";
+import coverPhoto from "../public/assets/accord-spring-446.jpg";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <main className={styles.main} >
-        <div className={styles.article} >
-          <div className={styles.home_cover}><Image alt="Catskills" src={accordSring209} layout="fill" objectFit='cover' /></div>
-          <div className={styles.home_cover_overlay}><h2 id="title" >
-            bone hollow is a boutique development company located in Ulster County
-          </h2>
+      <main className={styles.main}>
+        <article className={styles.article}>
+          {" "}
+          <div className={styles.home_cover}>
+            {" "}
+            <Image
+              alt="Catskills"
+              src={coverPhoto}
+              layout="fill"
+              objectFit="cover"
+            />
           </div>
-          <section className="flexRow" id="para1">
-            <div className="flexRow">
-              <p id="para1Text">
-                each home is designed and built with the surroundings taking the
-                lead role
-              </p>
-            </div>
-          </section>
+        </article>
+      </main>
+      <main className={styles.main}>
+        <article className={styles.article}>
+          <div className={styles.home_cover_overlay}>
+            <h2 id="title">
+              bone hollow is a boutique development company located in Ulster
+              County
+            </h2>
+          </div>
+        </article>
+      </main>
+      <main className={styles.main}>
+        <article className={styles.article}>
+          <div className="flexRow">
+            <p id="para1Text">
+              each home is designed and built with the surroundings taking the
+              lead role
+            </p>
+          </div>
+        </article>
+        <article className={styles.article}>
           <div id="para2">
-            <p>the result is a rendezvous of rural tranquility and custom design</p>
+            <p>
+              the result is a rendezvous of rural tranquility and custom design
+            </p>
           </div>
           <div id="para3">
             <p>
               we love living in the Hudson Valley and want to help maintain its
-              special rural community<br />
+              special rural community
+              <br />
               there's something about this place that sparks creativity and
               innovation
             </p>
           </div>
-        </div>
+        </article>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
