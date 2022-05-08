94b0b8a1ad4fb27357b7423c3d023f9303d5325a
nworb9 <emma.nworb@gmail.com>
2022-05-07 21:04:16 -0400

    Init branch with a few grid improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import pigPic from "../public/assets/sample.jpeg";
-import backyard from "../public/assets/accord-fall-172.jpg";
+import backyard from "../public/assets/accord-spring-209.jpg";
 import coverPhoto from "../public/assets/accord-spring-446.jpg";
 
 const Home: NextPage = () => {
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <article className={styles.article}>
           <div className={styles.home_cover_overlay}>
-            <h2 id="title">
+            <h2 id="heading">
               bone hollow is a boutique development company located in Ulster
               County
             </h2>
@@ -35,20 +35,27 @@ const Home: NextPage = () => {
       </main>
       <main className={styles.main}>
         <article className={styles.article}>
-          <div className="flexRow">
-            <p id="para1Text">
-              each home is designed and built with the surroundings taking the
-              lead role
-            </p>
-          </div>
+          <Image
+            alt="poolhouse shot"
+            src={backyard}
+            width={500}
+            height={500}
+            objectFit="contain"
+          />
         </article>
         <article className={styles.article}>
+          <p id="para1">
+            each home is designed and built with the surroundings taking the
+            lead role
+          </p>
+        </article>
+      </main>
+      <main className={styles.main}>
+        <article className={styles.article}>
+          <p>
+            the result is a rendezvous of rural tranquility and custom design
+          </p>
           <div id="para2">
-            <p>
-              the result is a rendezvous of rural tranquility and custom design
-            </p>
-          </div>
-          <div id="para3">
             <p>
               we love living in the Hudson Valley and want to help maintain its
               special rural community
