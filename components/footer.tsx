e49cd7ada3d16c43d6e06cbfb5d6ce7d1e648572
nworb9 <emma.nworb@gmail.com>
2022-05-15 11:32:47 -0400

    Added grid in homes page

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,24 +1,35 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Footer() {
-    return (
-      <>
+  return (
+    <>
       <footer className={styles.footer}>
-        <div className={styles.footer_title}><p>Bone Hollow</p></div>
+        <div className={styles.footer_title}>
+          <p>Bone Hollow</p>
+        </div>
         <div className={styles.footer_contact}>
           <p>Contact</p>
-          <Link href={'mailto:erika@bonehollow.studio'}> erika@bonehollow.studio</Link>
+          /* eslint-disable */
+          <Link href={"mailto:erika@bonehollow.studio"}>
+            erika@bonehollow.studio
+          </Link>
+          /* eslint-enable */
         </div>
         <div className={styles.footer_social}>
           <p>Social</p>
-          <Link href={'https://www.instagram.com/bonehollowstudio/'} passHref><a target="_blank" rel="noopener noreferrer">instagram</a></Link>
+          <Link href={"https://www.instagram.com/bonehollowstudio/"} passHref>
+            <a target="_blank" rel="noopener noreferrer">
+              instagram
+            </a>
+          </Link>
           <p>facebook</p>
         </div>
-        <div className={styles.footer_copyright}>© Copyright Bone Hollow  LLC</div>
+        <div className={styles.footer_copyright}>
+          © Copyright Bone Hollow LLC
+        </div>
       </footer>
-      </>
-    )
-  }
+    </>
+  );
+}
