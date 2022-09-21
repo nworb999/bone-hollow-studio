eb981f5e88075ee2fdb1b7594f7109d6891ccb10
nworb9 <emma.nworb@gmail.com>
2022-09-20 22:45:39 -0400

    Fixed transparent burgermenu

--- components/burgermenu.tsx
+++ components/burgermenu.tsx
@@ -1,35 +1,35 @@
 import { LayoutProps } from "../src/types";
 import Link from "next/link";
-import Image from "next/image";
 import { slide as Menu, State } from "react-burger-menu";
 import navStyles from "../styles/navbar.module.css";
-import styles from "../styles/footer.module.css";
 import { useRouter } from "next/router";
-
 import React, { useState, useContext } from "react";
-import { aboutImageArray } from "../src/constants";
-import { CenterContent, LeftContent, RightContent } from "./containers";
+
 interface MenuState extends State {
   toggleMenu?: () => void;
   stateChangeHandler?: (newState: MenuState) => void;
 }
 
 // hamburger menu
-const HamburgerIcon = () => (
-  <svg
-    className="w-8 h-8"
-    fill="none"
-    color="#263430"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    height="3rem"
-    strokeWidth="2"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path d="M4 6h16M4 12h16M4 18h16"></path>
-  </svg>
-);
+const HamburgerIcon = () => {
+  const ctx = useContext(MyContext);
+  return (
+    <svg
+      className="w-8 h-8"
+      fill="none"
+      color="#263430"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="3rem"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path d="M4 6h16M4 12h16M4 18h16"></path>
+      <rect d="M4 6h16M4 12h16M4 18h16" onClick={ctx.toggleMenu} />
+    </svg>
+  );
+};
 
 // make a new context
 const MyContext = React.createContext<MenuState>({ isOpen: false });
@@ -69,7 +69,7 @@ const Navigation = ({ children }: LayoutProps) => {
     <div className={navStyles.burger_icon}>
       <Menu
         noOverlay
-        disableOverlayClick
+        // disableOverlayClick
         customBurgerIcon={<HamburgerIcon />}
         isOpen={ctx.isOpen}
         onStateChange={(state) => ctx.stateChangeHandler?.(state)}
