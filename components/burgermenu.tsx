7e0eb2dcf9e5b9cba838d2c49ce5702eee1f668f
nworb9 <emma.nworb@gmail.com>
2022-09-24 14:39:01 -0400

    Fixed cut-off

--- components/burgermenu.tsx
+++ components/burgermenu.tsx
@@ -47,20 +47,18 @@ const HomeButton = () => {
 const Navigation = ({ children }: LayoutProps) => {
   const ctx = useContext(MyContext);
   return (
-    <div className={navStyles.burger_icon}>
-      <Menu
-        noOverlay
-        customBurgerIcon={<HamburgerIcon />}
-        isOpen={ctx.isOpen}
-        onStateChange={ctx.toggleMenu}
-        width={"100%"}
-        pageWrapId={"page-wrap"}
-        outerContainerId={"outer-container"}
-        burgerButtonClassName={navStyles.burger}
-      >
-        {children}
-      </Menu>
-    </div>
+    <Menu
+      noOverlay
+      customBurgerIcon={<HamburgerIcon />}
+      isOpen={ctx.isOpen}
+      onStateChange={ctx.toggleMenu}
+      width={"100%"}
+      pageWrapId={"page-wrap"}
+      outerContainerId={"outer-container"}
+      burgerButtonClassName={navStyles.burger}
+    >
+      {children}
+    </Menu>
   );
 };
 
