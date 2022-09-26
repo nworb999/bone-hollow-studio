2193188bb68c9b48ade1b12b3c33bf0b368cb52f
nworb9 <emma.nworb@gmail.com>
2022-09-26 09:13:48 -0400

    added document page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,15 +10,30 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link
-          rel="icon"
-          type="image/x-icon"
-          href="/leaf.svg"
+          rel="shortcut icon"
+          href="/images/leaf.svg"
           className="invert(68%) sepia(9%) saturate(467%) hue-rotate(64deg) brightness(93%) contrast(89%)"
-        ></link>
+        />
         <link
-          href="https://fonts.googleapis.com/css2?family=Chivo&family=Inconsolata&family=Open+Sans&display=swap"
-          rel="stylesheet"
-        ></link>
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/leaf.svg"
+          className="invert(68%) sepia(9%) saturate(467%) hue-rotate(64deg) brightness(93%) contrast(89%)"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/leaf.svg"
+          className="invert(68%) sepia(9%) saturate(467%) hue-rotate(64deg) brightness(93%) contrast(89%)"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/leaf.svg"
+          className="invert(68%) sepia(9%) saturate(467%) hue-rotate(64deg) brightness(93%) contrast(89%)"
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
